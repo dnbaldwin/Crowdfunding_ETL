81e511646c9384d277cd76109ef38cb9baf7bc6b
dnbaldwin <david.baldwin@ymail.com>
2023-07-03 16:34:27 +0930

    Project 2 Commit - Final

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Crowdfunding_ETL
 Project_2
+
+This project was done alone (Clinton and I had been paired but had not realised it was designed as a shared task)
+
+The task is incomplete, as I have been unable to load data in to the SQL tables set up by the schema and despite making a number of changes to constraints I have not been successful in getting the tables to load.
+
+As the project is well overdue, I have elected to submit as is.
+
+The jupyter notebook for the project is ETL_Mini_Project_Dbaldwin
+
+Thanks for your time in reviewing this project
---
+++ Resources/QuickDBD-campaign.sql
@@ -0,0 +1,69 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/GmXDgX
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Campaign" (
+    "cf_id" Integer   NOT NULL,
+    "contact_id" Integer   NOT NULL,
+    "company_name" Varchar(30)   NOT NULL,
+    "description" Varchar(100)   NOT NULL,
+    "goal" Float   NOT NULL,
+    "pledged" Integer   NOT NULL,
+    "outcome" Varchar(30)   NOT NULL,
+    "backers_count" Integer   NOT NULL,
+    "country" Varchar(30)   NOT NULL,
+    "currency" Varchar(10)   NOT NULL,
+    "launch_date" Varchar(30)   NOT NULL,
+    "end_date" Varchar(30)   NOT NULL,
+    "category_id" Varchar(30)   NOT NULL,
+    "subcategory_id" Varchar(30)   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+     ),
+    CONSTRAINT "uc_Campaign_contact_id" UNIQUE (
+        "contact_id"
+    ),
+    CONSTRAINT "uc_Campaign_category_id" UNIQUE (
+        "category_id"
+    ),
+    CONSTRAINT "uc_Campaign_subcategory_id" UNIQUE (
+        "subcategory_id"
+    )
+);
+
+CREATE TABLE "Category" (
+    "category_id" Varchar(30)   NOT NULL,
+    "category" Varchar(30)   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" Varchar(30)   NOT NULL,
+    "subcategory" Varchar(30)   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "Contacts" (
+    "contact_id" Integer   NOT NULL,
+    "firstname" Varchar(30)   NOT NULL,
+    "lastname" Varchar(30)   NOT NULL,
+    "email" Varchar(30)   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "Category" ADD CONSTRAINT "fk_Category_category_id" FOREIGN KEY("category_id")
+REFERENCES "Campaign" ("category_id");
+
+ALTER TABLE "Subcategory" ADD CONSTRAINT "fk_Subcategory_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Campaign" ("subcategory_id");
+
+ALTER TABLE "Contacts" ADD CONSTRAINT "fk_Contacts_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Campaign" ("contact_id");
+
